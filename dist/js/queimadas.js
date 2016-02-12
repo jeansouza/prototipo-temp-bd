a0a4106fd5d7cdaeb7811dcf52ba47b3d1e013a6
jeansouza <jeancfsouza@gmail.com>
2016-02-12 15:04:58 -0200

    -

--- dist/js/queimadas.js
+++ dist/js/queimadas.js
@@ -3,19 +3,34 @@ $(".sidebar-toggle").on('click', function(){
     $("#reduced-footer > div").animate({ height: '4px' }, 300);
     $("#footer").hide();
     $("#extra-footer").animate({ height: '4px' }, 300);
+
+    $("#gov-header").animate({ 'margin-top': '-182px' }, 300);
+
+    $(".main-sidebar").animate({ 'padding-top': '50px' }, 300);
+
+    $('.content-wrapper').attr("style", "min-height: " + ($(window).height() - 62) + "px");
+    $('#map').attr("style", "height: " + ($(window).height() - 116) + "px");
   } else {
     $("#reduced-footer > div").animate({ height: '0px' }, 300);
     $("#footer").show();
     $("#extra-footer").animate({ height: '100%' }, 300);
+
+    $("#gov-header").animate({ 'margin-top': '0' }, 300);
+
+    $(".main-sidebar").animate({ 'padding-top': '232px' }, 300);
+
+    $('.content-wrapper').attr("style", "min-height: " + ($(window).height() - 244) + "px");
+    $('#map').attr("style", "height: " + ($(window).height() - 298) + "px");
   }
 });
+
 
 // guardar valores em uso
 var h = $(window).height(),
     w = $(window).width();
 
-$('.content-wrapper').attr("style", "min-height: " + (h - 95) + "px");
-$('#map').attr("style", "height: " + (h - 149) + "px");
+$('.content-wrapper').attr("style", "min-height: " + (h - 244) + "px");
+$('#map').attr("style", "height: " + (h - 298) + "px");
 
 // realizar verificação
 $(window).resize(function(){
@@ -28,8 +43,8 @@ $(window).resize(function(){
      */
 
     if (nh != h) {
-      $('.content-wrapper').attr("style", "min-height: " + (nh - 95) + "px");
-      $('#map').attr("style", "height: " + (nh - 149) + "px");
+      $('.content-wrapper').attr("style", "min-height: " + (nh - 244) + "px");
+      $('#map').attr("style", "height: " + (nh - 298) + "px");
     }
 
     // atualizar os valores nas variáveis que guardam o valor antigo
